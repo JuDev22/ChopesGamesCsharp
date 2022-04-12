dcc03008d481c5827575a19ee56f7814df144535
JuDev22 <b.julian.pro@icloud.com>
2022-04-12 21:05:03 +0200

    Lister les produits presque finit

---
+++ ChopesGames/FormListerProduits.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ChopesGames
+{
+    partial class FormListerProduits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCategorie = new System.Windows.Forms.Label();
+            this.lblMarque = new System.Windows.Forms.Label();
+            this.cmbCategorie = new System.Windows.Forms.ComboBox();
+            this.cmbMarque = new System.Windows.Forms.ComboBox();
+            this.lvProduits = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Recherche dans le catalogue par :";
+            // 
+            // lblCategorie
+            // 
+            this.lblCategorie.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCategorie.AutoSize = true;
+            this.lblCategorie.Location = new System.Drawing.Point(232, 56);
+            this.lblCategorie.Name = "lblCategorie";
+            this.lblCategorie.Size = new System.Drawing.Size(52, 13);
+            this.lblCategorie.TabIndex = 1;
+            this.lblCategorie.Text = "Catégorie";
+            // 
+            // lblMarque
+            // 
+            this.lblMarque.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMarque.AutoSize = true;
+            this.lblMarque.Location = new System.Drawing.Point(232, 81);
+            this.lblMarque.Name = "lblMarque";
+            this.lblMarque.Size = new System.Drawing.Size(43, 13);
+            this.lblMarque.TabIndex = 2;
+            this.lblMarque.Text = "Marque";
+            // 
+            // cmbCategorie
+            // 
+            this.cmbCategorie.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbCategorie.FormattingEnabled = true;
+            this.cmbCategorie.Location = new System.Drawing.Point(303, 53);
+            this.cmbCategorie.Name = "cmbCategorie";
+            this.cmbCategorie.Size = new System.Drawing.Size(131, 21);
+            this.cmbCategorie.TabIndex = 5;
+            this.cmbCategorie.SelectedIndexChanged += new System.EventHandler(this.cmbCategorie_SelectedIndexChanged);
+            // 
+            // cmbMarque
+            // 
+            this.cmbMarque.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbMarque.FormattingEnabled = true;
+            this.cmbMarque.Location = new System.Drawing.Point(303, 78);
+            this.cmbMarque.Name = "cmbMarque";
+            this.cmbMarque.Size = new System.Drawing.Size(131, 21);
+            this.cmbMarque.TabIndex = 6;
+            this.cmbMarque.SelectedIndexChanged += new System.EventHandler(this.cmbMarque_SelectedIndexChanged);
+            // 
+            // lvProduits
+            // 
+            this.lvProduits.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvProduits.HideSelection = false;
+            this.lvProduits.Location = new System.Drawing.Point(12, 128);
+            this.lvProduits.Name = "lvProduits";
+            this.lvProduits.Size = new System.Drawing.Size(634, 311);
+            this.lvProduits.TabIndex = 10;
+            this.lvProduits.UseCompatibleStateImageBehavior = false;
+            // 
+            // FormListerProduits
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 451);
+            this.Controls.Add(this.lvProduits);
+            this.Controls.Add(this.cmbMarque);
+            this.Controls.Add(this.cmbCategorie);
+            this.Controls.Add(this.lblMarque);
+            this.Controls.Add(this.lblCategorie);
+            this.Controls.Add(this.label1);
+            this.Name = "FormListerProduits";
+            this.Text = "Lister les produits";
+            this.Load += new System.EventHandler(this.FormListerProduits_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCategorie;
+        private System.Windows.Forms.Label lblMarque;
+        private System.Windows.Forms.ComboBox cmbCategorie;
+        private System.Windows.Forms.ComboBox cmbMarque;
+        private System.Windows.Forms.ListView lvProduits;
+    }
+}
---
+++ ChopesGames/FormListerProduits.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
+
+namespace ChopesGames
+{
+    public partial class FormListerProduits : Form
+    {
+        private MySqlConnection sqlcon;
+        public FormListerProduits()
+        {
+            InitializeComponent();
+            sqlcon = new MySqlConnection("SERVER=127.0.0.1; DATABASE=chopesgames; UID=root; PASSWORD=; Convert Zero Datetime = true;");
+        }
+        void ChargerListeProduits(string requete)
+        {
+            // initialisation de la listViev lignes de commande lvProduits
+            lvProduits.View = View.Details; // Mode d'affichage
+            lvProduits.GridLines = true; // on affichera la grille
+            lvProduits.FullRowSelect = true; // Mode de sélection : ligne
+                                             // Pour le mode de selection voir aussi la property SelectionMode
+            lvProduits.Columns.Add("Date", 80);
+            lvProduits.Columns.Add("Libelle", 150);
+            lvProduits.Columns.Add("Prix HT", 80);
+            lvProduits.Columns.Add("Taux TVA", 80);
+            lvProduits.Columns.Add("Quantitée", 70);
+            lvProduits.Columns.Add("Disponible", 70);
+            lvProduits.Columns.Add("Vitrine", 50);
+            try
+            {
+                MySqlDataReader jeuEnr = null;
+                MySqlCommand sqlcomProduits;
+                sqlcon.Open();
+                MessageBox.Show(requete);
+                sqlcomProduits = new MySqlCommand(requete, sqlcon);                
+                jeuEnr = sqlcomProduits.ExecuteReader();
+                var TabItem = new string[7];
+                while (jeuEnr.Read())
+                {
+
+                    TabItem[0] = jeuEnr.GetDateTime("DATEAJOUT").ToString();
+                    TabItem[1] = jeuEnr.GetString("LIBELLE");
+                    TabItem[2] = jeuEnr.GetDouble("PRIXHT").ToString();
+                    TabItem[3] = jeuEnr.GetDouble("TAUXTVA").ToString();
+                    TabItem[4] = jeuEnr.GetInt32("QUANTITEENSTOCK").ToString();
+                    TabItem[5] = jeuEnr.GetInt32("DISPONIBLE").ToString();
+                    TabItem[6] = jeuEnr.GetInt32("Vitrine").ToString();
+                    lvProduits.Items.Add(new ListViewItem(TabItem));
+                }
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur chargement des produits : " + erreur.ToString());
+            }
+            finally
+            {
+                if (sqlcon is object & sqlcon.State == ConnectionState.Open)
+                {
+                    sqlcon.Close(); // on se déconnecte
+                }
+            }
+
+        }
+        // Au chargement, declaration de la requete puis envoie dans charger produit
+        private void FormListerProduits_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string requête;
+                int noCategorie;
+                string libelle;
+                MySqlDataReader jeuEnr = null;
+                sqlcon.Open(); // on se connecte
+                requête = "Select * from Categorie";
+                var maCde = new MySqlCommand(requête, sqlcon);
+                jeuEnr = maCde.ExecuteReader();
+
+                while (jeuEnr.Read())
+                {
+                    noCategorie = jeuEnr.GetInt32("NOCATEGORIE");
+                    libelle = jeuEnr.GetString("LIBELLE");
+                    cmbCategorie.Items.Add(new Categorie(noCategorie, libelle));
+                }
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur chargement catégories : " + erreur.ToString());
+            }
+            finally
+            {
+                if (sqlcon is object & sqlcon.State == ConnectionState.Open)
+                {
+                    sqlcon.Close(); // on se déconnecte
+                }
+            }
+
+            // Chargement des marques dans cmbMarque
+            try
+            {
+                string requête;
+                int noMarque;
+                string nom;
+                MySqlDataReader jeuEnr = null;
+                sqlcon.Open(); // on se connecte
+                requête = "Select * from Marque";
+                var maCde = new MySqlCommand(requête, sqlcon);
+                jeuEnr = maCde.ExecuteReader();
+
+                while (jeuEnr.Read())
+                {
+                    noMarque = jeuEnr.GetInt32("NOMARQUE");
+                    nom = jeuEnr.GetString("NOM");
+                    cmbMarque.Items.Add(new Marque(noMarque, nom));
+                }
+            }
+            catch (MySqlException erreur)
+            {
+                MessageBox.Show("Erreur chargement marques : " + erreur.ToString());
+            }
+            finally
+            {
+                if (sqlcon is object & sqlcon.State == ConnectionState.Open)
+                {
+                    sqlcon.Close(); // on se déconnecte
+                }
+            }
+            // Chargement des produits
+            string requete = "Select DATEAJOUT,LIBELLE,PRIXHT,TAUXTVA,QUANTITEENSTOCK,DISPONIBLE,VITRINE from Produit";
+            ChargerListeProduits(requete);
+
+        }
+        // Si l'élément sélectionné de la cmbCategorie est changer, envoie d'une nouvelle requete
+        private void cmbCategorie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int noCategorie = ((Categorie)(cmbCategorie.SelectedItem)).GetNoCategorie();
+            int noMarque = ((Marque)(cmbMarque.SelectedItem)).GetNoMarque();
+            foreach (Categorie categorie in cmbCategorie.Items)
+            {
+                if (categorie.GetNoCategorie() == noCategorie)
+                {
+                    cmbCategorie.SelectedItem = categorie;
+                }
+            }
+            foreach (Marque marque in cmbMarque.Items)
+            {
+                if (marque.GetNoMarque() == noMarque)
+                {
+                    cmbMarque.SelectedItem = marque;
+                }
+            }
+            string requete;
+            // Cas ou noCategorie true et noMarque false
+            if (cmbCategorie.Text != null & cmbMarque.Text == null)
+            {
+                requete = "Select DATEAJOUT,LIBELLE,PRIXHT,TAUXTVA,QUANTITEENSTOCK,DISPONIBLE,VITRINE from Produit WHERE NOCATEGORIE = " + noCategorie;
+                ChargerListeProduits(requete);
+            }
+            // Cas ou noCategorie false et noMarque true
+            else if (cmbCategorie.Text == null & cmbMarque.Text != null)
+            {
+                requete = "Select DATEAJOUT,LIBELLE,PRIXHT,TAUXTVA,QUANTITEENSTOCK,DISPONIBLE,VITRINE from Produit WHERE NOMARQUE = " + noMarque;
+                ChargerListeProduits(requete);
+            }
+            // Cas ou noCategorie true et noMarque true
+            else
+            {
+                requete = "Select DATEAJOUT,LIBELLE,PRIXHT,TAUXTVA,QUANTITEENSTOCK,DISPONIBLE,VITRINE from Produit WHERE NOCATEGORIE = " + noCategorie + "AND NOMARQUE = " + noMarque;
+                ChargerListeProduits(requete);
+            }
+        }
+
+        private void cmbMarque_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int noCategorie = ((Categorie)(cmbCategorie.SelectedItem)).GetNoCategorie();
+            int noMarque = ((Marque)(cmbMarque.SelectedItem)).GetNoMarque();
+            foreach (Categorie categorie in cmbCategorie.Items)
+            {
+                if (categorie.GetNoCategorie() == noCategorie)
+                {
+                    cmbCategorie.SelectedItem = categorie;
+                }
+            }
+            foreach (Marque marque in cmbMarque.Items)
+            {
+                if (marque.GetNoMarque() == noMarque)
+                {
+                    cmbMarque.SelectedItem = marque;
+                }
+            }
+            string requete;
+            // Cas ou noCategorie true et noMarque false
+            if (cmbCategorie.Text != null & cmbMarque.Text == null)
+            {
+                requete = "Select DATEAJOUT,LIBELLE,PRIXHT,TAUXTVA,QUANTITEENSTOCK,DISPONIBLE,VITRINE from Produit WHERE NOCATEGORIE = " + noCategorie;
+                ChargerListeProduits(requete);
+            }
+            // Cas ou noCategorie false et noMarque true
+            else if (cmbCategorie.Text == null & cmbMarque.Text != null)
+            {
+                requete = "Select DATEAJOUT,LIBELLE,PRIXHT,TAUXTVA,QUANTITEENSTOCK,DISPONIBLE,VITRINE from Produit WHERE NOMARQUE = " + noMarque;
+                ChargerListeProduits(requete);
+            }
+            // Cas ou noCategorie true et noMarque true
+            else
+            {
+                requete = "Select DATEAJOUT,LIBELLE,PRIXHT,TAUXTVA,QUANTITEENSTOCK,DISPONIBLE,VITRINE from Produit WHERE NOCATEGORIE = " + noCategorie + "AND NOMARQUE = " + noMarque;
+                ChargerListeProduits(requete);
+            }
+        }
+    }
+}
